8733f1ef5908ad4164ba6c7a3bf45b415df6b311
Mathias Maciossek <mathias.maciossek@gmail.com>
2019-04-04 14:32:44 +0200

    Improved the code a little

--- components/index/index.controller.js
+++ components/index/index.controller.js
@@ -1,93 +1,117 @@
-const unionBy = require('lodash/unionBy')
-const uniq = require('lodash/uniq')
-const { createApolloFetch } = require('apollo-fetch');
-const { normalize, schema } = require('normalizr');
+const unionBy = require("lodash/unionBy");
+const { createApolloFetch } = require("apollo-fetch");
 
-const CLASSROOM_ORG_NAME = process.env.CLASSROOM_ORG_NAME
-const TEACHER_ID = process.env.TEACHER_ID
+const CLASSROOM_ORG_NAME = process.env.CLASSROOM_ORG_NAME;
+const TEACHER_ID = process.env.TEACHER_ID;
+
+const assignmentNames = [
+  "hft-asgmt-git",
+  "hft-asgmt-html",
+  "hft-asgmt-css",
+  "hft-asgmt-frontend-development",
+  "hft-asgmt-js",
+  "hft-asgmt-nodejs",
+  "hft-asgmt-js-async"
+];
 
 async function indexController(req, res) {
   const apolloFetch = createApolloFetch({
-    uri: 'https://api.github.com/graphql',
+    uri: "https://api.github.com/graphql"
   });
 
   apolloFetch.use(({ request, options }, next) => {
     if (!options.headers) {
-      options.headers = {};  // Create the headers object if needed.
+      options.headers = {}; // Create the headers object if needed.
     }
-    options.headers['authorization'] = `Bearer ${req.cookies.ghAccessToken}`;
-  
+    options.headers["authorization"] = `Bearer ${req.cookies.ghAccessToken}`;
+
     next();
   });
 
-  let repos = []
+  let repos = [];
 
   try {
-    repos = await fetchAllRepos(apolloFetch)
-    const normalizedJson = normalizeRepos(repos)
-  
-    res.render('index/index', { CLASSROOM_ORG_NAME, data: normalizedJson });
+    repos = await fetchAllRepos(apolloFetch);
+
+    res.render("index/index", {
+      CLASSROOM_ORG_NAME,
+      data: normalizeRepos(repos),
+      assignmentNames
+    });
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
 function normalizeRepos(originalData) {
-  let users = []
-  let userAssignments = []
+  const parsedData = new Map();
 
-  const dataWithoutMe = originalData
+  originalData
+    .filter(item => item.node.url.indexOf("maciossek") === -1)
+    .forEach(item => {
+      const userObj = {
+        user: item.node.collaborators.nodes.filter(
+          user => user.login !== "maciossek"
+        )[0],
+        assignments: [{ name: item.node.name, url: item.node.url }]
+      };
+      if (!parsedData.has(userObj.user.login)) {
+        parsedData.set(userObj.user.login, userObj);
+      } else {
+        const storedUser = parsedData.get(userObj.user.login);
+        const newAssignment = {
+          name: item.node.name,
+          url: item.node.url
+        };
 
-  dataWithoutMe.forEach((item => {
-    repoName = item.node.name
-
-    const user = item.node.collaborators.nodes
-    const userAssignment = [{
-      name: item.node.name,
-      url: item.node.url,
-      user: typeof user[1] !== 'undefined' ? user[1].login : user[0].login
-    }]
-    
-    users = unionBy(users, user, 'login')
-    userAssignments = unionBy(userAssignments, userAssignment, 'name')
-  }))
-
-  users.forEach((item => {
-    item.repos = []
-    userAssignments.forEach(ua => {
-      // console.log(ua.user === item.login, ua.user === item.login)
-      if (ua.user === item.login) {
-        item.repos.push(ua)
+        assignmentNames.indexOf("item.node.name") >=
+        assignmentNames.indexOf(
+          storedUser.assignments[storedUser.assignments.length - 1].name
+        )
+          ? storedUser.assignments.push(newAssignment)
+          : storedUser.assignments.unshift(newAssignment);
       }
-    })
-  }))
-
-  return users;
+    });
+  return Array.from(parsedData.values());
 }
 
 async function fetchAllRepos(apolloFetch) {
-  let allRepos = []
-  let assignments = await fetchAssignments(apolloFetch)
-  let hasNextPage = assignments.data.organization.repositories.pageInfo.hasNextPage
-  let endCursor = assignments.data.organization.repositories.pageInfo.endCursor
-  allRepos.push(...assignments.data.organization.repositories.edges)
+  let allRepos = [];
+  let assignments = await fetchAssignments(apolloFetch);
+  if (!assignments.data) {
+    throw new Error(assignments.message);
+  }
+  let hasNextPage =
+    assignments.data.organization.repositories.pageInfo.hasNextPage;
+  let endCursor = assignments.data.organization.repositories.pageInfo.endCursor;
+  allRepos.push(...assignments.data.organization.repositories.edges);
 
-  while(hasNextPage) {
-    const nextAssignments = await fetchNextAssignments(apolloFetch, endCursor)
-    console.log(apolloFetch)
-    allRepos.push(...nextAssignments.data.organization.repositories.edges)
-    hasNextPage = nextAssignments.data.organization.repositories.pageInfo.hasNextPage
-    endCursor = nextAssignments.data.organization.repositories.pageInfo.endCursor
+  while (hasNextPage) {
+    let nextAssignments;
+
+    try {
+      nextAssignments = await fetchNextAssignments(apolloFetch, endCursor);
+      if (!nextAssignments.data) {
+        throw new Error(nextAssignments.message);
+      }
+      allRepos.push(...nextAssignments.data.organization.repositories.edges);
+      hasNextPage =
+        nextAssignments.data.organization.repositories.pageInfo.hasNextPage;
+      endCursor =
+        nextAssignments.data.organization.repositories.pageInfo.endCursor;
+    } catch (e) {
+      throw e;
+    }
   }
 
-  return allRepos
+  return allRepos;
 }
 
 async function fetchAssignments(apolloFetch) {
   return apolloFetch({
     query: `query($CLASSROOM_ORG_NAME:String!) {
       organization(login: $CLASSROOM_ORG_NAME) {
-        repositories(first: 2) {
+        repositories(first: 100) {
           edges {
             node {
               id
@@ -112,16 +136,16 @@ async function fetchAssignments(apolloFetch) {
       }
     }`,
     variables: {
-      "CLASSROOM_ORG_NAME": CLASSROOM_ORG_NAME
-    },
-  })
+      CLASSROOM_ORG_NAME: CLASSROOM_ORG_NAME
+    }
+  });
 }
 
 async function fetchNextAssignments(apolloFetch, pageCursor) {
   return apolloFetch({
     query: `query($CLASSROOM_ORG_NAME:String! $pageCursor:String!) {
       organization(login: $CLASSROOM_ORG_NAME) {
-        repositories(first: 2, after: $pageCursor) {
+        repositories(first: 100, after: $pageCursor) {
           edges {
             node {
               id
@@ -146,10 +170,10 @@ async function fetchNextAssignments(apolloFetch, pageCursor) {
       }
     }`,
     variables: {
-      "CLASSROOM_ORG_NAME": CLASSROOM_ORG_NAME,
-      "pageCursor": pageCursor
+      CLASSROOM_ORG_NAME: CLASSROOM_ORG_NAME,
+      pageCursor: pageCursor
     }
-  })
+  });
 }
 
 module.exports = indexController;
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = process.env.PORT || 3000;
 const app = express();
-const passport = require('passport');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const GitHubStrategy = require('passport-github').Strategy;
-const indexController = require('./components/index/index.controller')
-const cookieParser = require('cookie-parser');
+const passport = require("passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const GitHubStrategy = require("passport-github").Strategy;
+const indexController = require("./components/index/index.controller");
+const cookieParser = require("cookie-parser");
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
@@ -21,59 +20,69 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  // console.log('Profile:', profile)
-  // console.log('AccessToken:',accessToken)
-  // console.log('RefreshToken:',refreshToken)
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    
-    
-    return done(null, Object.assign({}, profile, { ghAccessToken: accessToken }));
-  });
-}
-));
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function() {
+        return done(
+          null,
+          Object.assign({}, profile, { ghAccessToken: accessToken })
+        );
+      });
+    }
+  )
+);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/components'))
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/components"));
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cookieParser())
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cookieParser());
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/auth/github',
-  passport.authenticate('github', { scope: 'user public_repo repo repo_deployment repo:status read:repo_hook read:org read:public_key read:gpg_key' }));
+app.get(
+  "/auth/github",
+  passport.authenticate("github", {
+    scope:
+      "user public_repo repo repo_deployment repo:status read:repo_hook read:org read:public_key read:gpg_key"
+  })
+);
 
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.cookie("ghAccessToken", req.user.ghAccessToken);
+    res.redirect("/");
+  }
+);
 
-app.get('/auth/github/callback', 
-passport.authenticate('github', { failureRedirect: '/login' }),
-function(req, res) {
-  res.cookie('ghAccessToken', req.user.ghAccessToken)
-  res.redirect('/');
+app.get("/", ensureAuthenticated, indexController);
+
+app.get("/login", function(req, res) {
+  res.render("login/login");
 });
 
-app.get('/', ensureAuthenticated, indexController);
-
-app.get('/login', function(req, res) {
-  res.render('login/login');
-});
-
-app.get('/logout', function(req, res){
+app.get("/logout", function(req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 app.listen(port, function() {
- console.log(`Server listening on port ${port}…`)
+  console.log(`Server listening on port ${port}…`);
 });
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/auth/github')
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/auth/github");
 }
